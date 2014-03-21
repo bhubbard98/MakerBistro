0282f547f309ee536cccee5b1f3c467afac6c792
Drew Robinson <drewbie0307@hotmail.com>
2014-03-21 11:27:56 -0500

    Updated by Drew

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,6 +6,8 @@ class MenusController < ApplicationController
 
   def show
     @menu = Menu.find(params[:id])
+    @ingredients = @menu.ingredients.all
+    @ingredient = @menu.ingredients.new
   end
 
   def new
